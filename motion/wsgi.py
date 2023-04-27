2fb01bea49234d259e475e30278c35ddae14b9a4
ilyas <you@example.com>
2023-04-28 00:24:23 +0600

    motion

--- motion/wsgi.py
+++ motion/wsgi.py
@@ -8,14 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-path = '/home/ilias09/Motion/motion'
-if path not in sys.path:
-    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'motion.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'motion.settings')
 
 application = get_wsgi_application()
+
+app = application
