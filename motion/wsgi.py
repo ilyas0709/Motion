5d719bf377648f612b319c02727269a70da5840c
ilyas <you@example.com>
2023-04-27 20:09:02 +0600

    motion-study

--- motion/wsgi.py
+++ motion/wsgi.py
@@ -14,8 +14,8 @@ path = '/home/ilias09/Motion/motion'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'motion.settings'
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'motion.settings'
 
 application = get_wsgi_application()
